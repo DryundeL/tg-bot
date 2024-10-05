85bf1074691ef013b20db1cde43076487e39c86f
DryundeL <andromedich021@gmail.com>
2024-10-05 16:26:00 +0300

    Fix db config

--- src/db/db.config.ts
+++ src/db/db.config.ts
@@ -7,7 +7,7 @@ const pool = new Pool({
   host: configService.get("DB_HOST", "localhost"),
   port: parseInt(configService.get("DB_PORT"), 5432),
   database: configService.get("DB_DATABASE", "test"),
-  user: configService.get("DB_USER", "postgres"),
+  user: configService.get("DB_USERNAME", "postgres"),
   password: configService.get("DB_PASSWORD", "root"),
 });
 
